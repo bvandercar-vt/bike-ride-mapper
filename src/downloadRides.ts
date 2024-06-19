b60398f08ebb5ff3f3af6d241840dcb53b65d041
bvandercar-vt <blake.vandercar@validatek.com>
2024-06-19 16:14:06 -0600

    feat: get workouts

--- src/downloadRides.ts
+++ src/downloadRides.ts
@@ -8,6 +8,12 @@ const API_URL = 'https://mapmyride.api.ua.com'
 
 const DIR = 'ride_files'
 
+interface Link {
+  href: string
+  id?: string
+  name?: string
+}
+
 interface Route {
   city: string
   country: string
@@ -30,7 +36,46 @@ interface Route {
   max_elevation: string
   _links: Record<
     'alternate' | 'privacy' | 'self' | 'activity_types' | 'user' | 'thumbnail' | 'documentation',
-    Array<{ href: string; id?: string; name?: string }>
+    Link[]
+  >
+}
+
+interface Workout {
+  name: string
+  start_datetime: string
+  start_locale_timezone: string
+  created_datetime: string
+  updated_datetime: string
+  reference_key: string
+  source: string
+  attachments: Record<string, any>
+  sharing: Record<string, any>
+  notes: string
+  aggregates: {
+    distance_total: number
+    metabolic_energy_total: number
+    active_time_total: number
+    elapsed_time_total: number
+    steps_total: number
+    heartrate_min: number
+    heartrate_max: number
+    heartrate_avg: number
+    speed_min: number
+    speed_max: number
+    speed_avg: number
+    cadence_min: number
+    cadence_max: number
+    cadence_avg: number
+    power_min: number
+    power_max: number
+    power_avg: number
+  }
+  has_time_series: boolean
+  time_series: Record<string, any> // could type
+  activity_type: string
+  _links: Record<
+    'self' | 'route' | 'activity_type' | 'user' | 'privacy' | 'workout_attribution',
+    Link[]
   >
 }
 
@@ -50,34 +95,46 @@ const downloadAllRoutes = async (token: string, user_id: string) => {
     return response
   }
 
-  async function getRoutes(limit: number, offset: number) {
-    const response = await get(
-      `/v7.2/route/?` +
-        new URLSearchParams({
-          limit: String(limit),
-          offset: String(offset),
-          user: user_id,
-          order_by: 'date_created',
-        }),
-    )
-    const result = await response.json()
-    return result._embedded.routes as Route[]
+  async function getAll<T>(endpoint: string, key: string, params: Record<string, string>) {
+    async function getBatch(limit: number, offset: number) {
+      const response = await get(
+        `/v7.2/${endpoint}/?` +
+          new URLSearchParams({
+            limit: String(limit),
+            offset: String(offset),
+            ...params,
+          }),
+      )
+      const result = await response.json()
+      return result._embedded[key] as T[]
+    }
+
+    const items: T[] = []
+    while (items.length % 10 == 0) {
+      const newItems = await getBatch(10, items.length)
+      items.push(...newItems)
+    }
+
+    return items
   }
 
-  const rides: Route[] = []
-  while (rides.length % 10 == 0) {
-    const newRides = await getRoutes(10, rides.length)
-    rides.push(...newRides)
-  }
+  const routes = await getAll<Route>('route', 'routes', { user: user_id, order_by: 'date_created' })
+
+  const workouts = await getAll<Workout>('workout', 'workouts', {
+    user: user_id,
+    order_by: 'start_datetime',
+  })
+
+  console.log('# workouts', workouts.length)
 
   if (!fs.existsSync(DIR)) {
     fs.mkdirSync(DIR)
   }
 
-  rides.forEach(async (ride) => {
-    const response = await get(ride._links.alternate[0].href)
+  routes.forEach(async (route) => {
+    const response = await get(route._links.alternate[0].href)
     const result = await response.arrayBuffer()
-    fs.writeFileSync(`${DIR}/${sanitize(ride.name)}.kml`, Buffer.from(result))
+    fs.writeFileSync(`${DIR}/${sanitize(route.name)}.kml`, Buffer.from(result))
   })
 }
 
