24c43feb1c2521d3a37872004dab106c4ea9d185
bvandercar-vt <blake.vandercar@validatek.com>
2024-06-19 15:48:32 -0600

    refactor: simplify

--- src/downloadRides.ts
+++ src/downloadRides.ts
@@ -8,41 +8,35 @@ const API_URL = 'https://mapmyride.api.ua.com'
 
 const DIR = 'ride_files'
 
-const get_rides = async (token: string, user_id: string) => {
-  const get = async (limit: number, offset: number) => {
-    const response = await fetch(
-      `${API_URL}/v7.2/route/?` +
-        new URLSearchParams({
-          limit: String(limit),
-          offset: String(offset),
-          user: user_id,
-          order_by: 'date_created',
-        }),
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: token,
-        },
-      },
-    )
-    if (!response.ok)
-      throw new Error(
-        `Failed to get ride list! status: ${response.status}${response.statusText}${await response.text()}`,
-      )
-    const result = await response.json()
-    return result._embedded.routes
-  }
+interface Route {
+  city: string
+  country: string
+  state: string
+  starting_location: string
+  start_point_type: string
+  postal_code: string
+  distance: string
+  name: string
+  description: string
+  data_source: string
+  images: string
+  created_datetime: string
+  updated_datetime: string
+  points: string
+  climbs: string
+  total_ascent: string
+  total_descent: string
+  min_elevation: string
+  max_elevation: string
+  _links: Record<
+    'alternate' | 'privacy' | 'self' | 'activity_types' | 'user' | 'thumbnail' | 'documentation',
+    Array<{ href: string; id?: string; name?: string }>
+  >
+}
 
-  const rides = []
-
-  while (rides.length % 10 == 0) {
-    const new_rides = await get(10, rides.length)
-    rides.push(...new_rides)
-  }
-
-  rides.forEach(async (ride) => {
-    const response = await fetch(API_URL + ride._links.alternate[0].href, {
+const downloadAllRoutes = async (token: string, user_id: string) => {
+  async function get(endpoint: string) {
+    const response = await fetch(`${API_URL}${endpoint}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -51,12 +45,38 @@ const get_rides = async (token: string, user_id: string) => {
     })
     if (!response.ok)
       throw new Error(
-        `Failed to get ride list! status: ${response.status}${response.statusText}${await response.text()}`,
+        `Failed to get from endpoint ${endpoint}. Status: ${response.status} ${response.statusText} ${await response.text()}`,
       )
+    return response
+  }
+
+  async function getRoutes(limit: number, offset: number) {
+    const response = await get(
+      `/v7.2/route/?` +
+        new URLSearchParams({
+          limit: String(limit),
+          offset: String(offset),
+          user: user_id,
+          order_by: 'date_created',
+        }),
+    )
+    const result = await response.json()
+    return result._embedded.routes as Route[]
+  }
+
+  const rides: Route[] = []
+  while (rides.length % 10 == 0) {
+    const newRides = await getRoutes(10, rides.length)
+    rides.push(...newRides)
+  }
+
+  if (!fs.existsSync(DIR)) {
+    fs.mkdirSync(DIR)
+  }
+
+  rides.forEach(async (ride) => {
+    const response = await get(ride._links.alternate[0].href)
     const result = await response.arrayBuffer()
-    if (!fs.existsSync(DIR)) {
-      fs.mkdirSync(DIR)
-    }
     fs.writeFileSync(`${DIR}/${sanitize(ride.name)}.kml`, Buffer.from(result))
   })
 }
@@ -67,4 +87,4 @@ if (!MMR_AUTH_TOKEN || !MMR_USER_ID) throw new Error('need env file')
 console.log(MMR_AUTH_TOKEN)
 console.log(MMR_USER_ID)
 
-await get_rides(MMR_AUTH_TOKEN, MMR_USER_ID)
+await downloadAllRoutes(MMR_AUTH_TOKEN, MMR_USER_ID)
