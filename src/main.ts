f1ab8bf6e2a29cc34ed0cacf5bd1082aeae35c26
bvandercar-vt <blake.vandercar@validatek.com>
2024-06-20 15:28:34 -0600

    move checkbox on mobile

--- src/main.ts
+++ src/main.ts
@@ -41,8 +41,6 @@ tileLayer(`https://api.maptiler.com/maps/streets-v2-dark/{z}/{x}/{y}.png?key=${M
   tileSize: 512,
   zoomOffset: -1,
   minZoom: 1,
-  attribution:
-    '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
   crossOrigin: true,
 }).addTo(map)
 
