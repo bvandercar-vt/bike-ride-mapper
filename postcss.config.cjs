e8095622f97cfbc077b6e93e7a79a8b11f464f17
bvandercar-vt <blake.vandercar@validatek.com>
2025-01-24 00:10:00 +0000

    add postcss

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+// @ts-check
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    autoprefixer: {}, // Adds vendor prefixes for compatibility
+  },
+}
+module.exports = config
