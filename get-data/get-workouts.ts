d3b15465b5765fc378483b67a5ff8d0c3cc9fed8
bvandercar-vt <blake.vandercar@validatek.com>
2025-07-29 20:05:30 -0600

    fix workflow, get-data

--- get-data/get-workouts.ts
+++ get-data/get-workouts.ts
@@ -28,44 +28,37 @@ const sanityClient = createSanityClient({
   useCdn: true,
 })
 
-const downloadAllRoutes = async (user_id: string) => {
-  const workouts = await getWorkouts({ user_id })
-  console.log('# workouts', workouts.length)
+console.log('getting workouts from mapmyride...')
+const workouts = await getWorkouts({ user_id: MMR_USER_ID })
+console.log('# workouts', workouts.length)
 
-  await Promise.all(
-    workouts.map(async (workout) => {
-      const route: Route = await get(workout._links.route[0].href).then((r) => r.json())
-      const activityType: ActivityType = await get(workout._links.activity_type[0].href).then((r) =>
-        r.json(),
-      )
-      if (!Object.values(ActivityName).includes(activityType.name))
-        throw new Error(`unexpected activity name ${activityType.name}`)
-
-      const kmlText = await get(route._links.alternate[0].href).then((r) => r.text())
-      const kmlParsed = new DOMParser().parseFromString(kmlText)
-      const obj = {
-        geoJson: kmlToGeoJson(kmlParsed),
-        workout: _.omitBy(
-          workout,
-          (val, key) => _.isObject(val) && key !== 'aggregates',
-        ) as CustomWorkout['workout'],
-        route: _.omitBy(
-          route,
-          (val, key) => _.isObject(val) && key !== 'starting_location',
-        ) as CustomWorkout['route'],
-        activityType: _.omitBy(activityType, (val) =>
-          _.isObject(val),
-        ) as CustomWorkout['activityType'],
-      } satisfies CustomWorkout
-
-      await sanityClient.createOrReplace({
-        _type: 'workout',
-        _id: `workout-${DateTime.fromISO(obj.workout.start_datetime).toFormat('yyyy-LL-dd-HH-mm-ss')}`,
-        title: obj.workout.name,
-        ..._.mapValues(obj, (val) => JSON.stringify(val, null, 2)),
-      })
-    }),
+workouts.forEach(async (workout) => {
+  const route: Route = await get(workout._links.route[0].href).then((r) => r.json())
+  const activityType: ActivityType = await get(workout._links.activity_type[0].href).then((r) =>
+    r.json(),
   )
-}
+  if (!Object.values(ActivityName).includes(activityType.name))
+    throw new Error(`unexpected activity name ${activityType.name}`)
 
-await downloadAllRoutes(MMR_USER_ID)
+  const kmlText = await get(route._links.alternate[0].href).then((r) => r.text())
+  const kmlParsed = new DOMParser().parseFromString(kmlText)
+  const obj = {
+    geoJson: kmlToGeoJson(kmlParsed),
+    workout: _.omitBy(
+      workout,
+      (val, key) => _.isObject(val) && key !== 'aggregates',
+    ) as CustomWorkout['workout'],
+    route: _.omitBy(
+      route,
+      (val, key) => _.isObject(val) && key !== 'starting_location',
+    ) as CustomWorkout['route'],
+    activityType: _.omitBy(activityType, (val) => _.isObject(val)) as CustomWorkout['activityType'],
+  } satisfies CustomWorkout
+
+  await sanityClient.createOrReplace({
+    _type: 'workout',
+    _id: `workout-${DateTime.fromISO(obj.workout.start_datetime).toFormat('yyyy-LL-dd-HH-mm-ss')}`,
+    title: obj.workout.name,
+    ..._.mapValues(obj, (val) => JSON.stringify(val, null, 2)),
+  })
+})
