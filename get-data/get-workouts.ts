0d6f797123f8479e19715a4aa99a1f290bbf9409
bvandercar-vt <blake.vandercar@validatek.com>
2025-07-30 22:40:00 +0000

    fix get-data

--- get-data/get-workouts.ts
+++ get-data/get-workouts.ts
@@ -13,6 +13,7 @@ import { getEnv } from '../src/utils'
 // have to import after due to getting the config vars
 const mapMyRide = await import('./api/map-my-ride.api')
 const { sanityClient } = await import('./api/sanity.api')
+const { SanityTypes, ...sanity } = await import('../src/api/sanity.api')
 
 const { MMR_USER_ID } = getEnv('MMR_USER_ID')
 
@@ -20,23 +21,27 @@ console.log('getting workouts from mapmyride...')
 const workouts = await mapMyRide.getWorkouts({ user_id: MMR_USER_ID })
 console.log('# workouts', workouts.length)
 
+console.log('converting KML to GeoJson and uploading to Sanity..')
 await Promise.all(
-  workouts.map((workout) => async () => {
+  workouts.map(async (workout) => {
     try {
       const workoutDate = DateTime.fromISO(workout.start_datetime)
       const route: Route = await mapMyRide.get(workout._links.route[0].href).then((r) => r.json())
       const activityType: ActivityType = await mapMyRide
         .get(workout._links.activity_type[0].href)
         .then((r) => r.json())
+
       if (!Object.values(ActivityName).includes(activityType.name))
         throw new Error(`unexpected activity name ${activityType.name}`)
 
-      const kmlText = await mapMyRide.get(route._links.alternate[0].href).then((r) => r.text())
+      const kmlText = await mapMyRide
+        .get(route._links.alternate.find((l) => l.name == 'kml')!.href)
+        .then((r) => r.text())
       const kmlParsed = new DOMParser().parseFromString(kmlText)
       const geoJson = kmlToGeoJson(kmlParsed)
 
       await sanityClient.createOrReplace<SanityWorkoutResponse>({
-        _type: 'workout',
+        _type: SanityTypes.WORKOUT,
         _id: `workout-${workoutDate.toFormat('yyyy-LL-dd-HH-mm-ss')}`,
         title: workoutDate.toFormat('yyyy-LL-dd') + ' ' + workout.name,
         ..._.mapValues(
@@ -59,3 +64,10 @@ await Promise.all(
     }
   }),
 )
+
+const sanityWorkouts = await sanity.getWorkouts()
+if (!(sanityWorkouts.length == workouts.length)) {
+  throw new Error(
+    `should be same length. Workouts in Sanity: ${sanityWorkouts.length} Workouts in MMR: ${workouts.length}`,
+  )
+}
