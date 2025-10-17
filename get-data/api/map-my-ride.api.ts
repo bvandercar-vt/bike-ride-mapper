fbf2540d549da1b3f989d31028e5cdf89db16ea8
bvandercar-vt <blake.vandercar@validatek.com>
2025-10-16 22:18:42 -0600

    better error log handling

--- get-data/api/map-my-ride.api.ts
+++ get-data/api/map-my-ride.api.ts
@@ -6,20 +6,27 @@ const { MMR_AUTH_TOKEN } = getEnv('MMR_AUTH_TOKEN')
 const MMR_API_URL = 'https://mapmyride.api.ua.com'
 
 export async function get(endpoint: string) {
-  const response = await fetch(`${MMR_API_URL}${endpoint}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: MMR_AUTH_TOKEN,
-    },
-  })
-  if (!response.ok)
-    throw new Error(
-      `Failed to get from endpoint ${endpoint}. Status: ${response.status} ${response.statusText} ${await response.text()}`,
-    )
-  return response
-}
+  try {
+    const response = await fetch(`${MMR_API_URL}${endpoint}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: MMR_AUTH_TOKEN,
+      },
+    })
 
+    if (!response.ok) {
+      throw new Error(
+        `Failed to get from endpoint ${endpoint}\nStatus: ${response.status} ${response.statusText}\nResponse: ${await response.text()}`,
+      )
+    }
+
+    return response
+  } catch (err) {
+    console.error(`Error fetching ${endpoint}:`, err)
+    throw err
+  }
+}
 export async function getAll<T>(endpoint: string, key: string, params: Record<string, string>) {
   async function getBatch(limit: number, offset: number) {
     const result = await get(
